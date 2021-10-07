93fb84683e2e2c450dba8b5bb3d99bc6e942c2df
mtr0930 <71540277+mtr0930@users.noreply.github.com>
2021-10-07 12:07:34 +0900

    Create meaning_tag.py

---
+++ mecab/meaning_tag.py
@@ -0,0 +1,17 @@
+# 형태소 품사중 실질형태소에 해당하는 부분만 추출
+
+from konlpy.tag import Mecab
+
+def mecab_normalize(text):
+    # 실질 형태소 품사
+    meaning_tags = ["NNG", "NNP", "NNB", "NR", "NP", "VV", "VA", "VX", "VCP", "VCN", "MM", "MAG", "MAJ", "IC"]
+    # 딕셔너리 파일 위치는 사용자 별로 상이
+    m = Mecab('C:\\mecab\\mecab-ko-dic')
+    out = m.pos(text)
+    # 실질 형태소만 담는 배열
+    meaning_words = []
+    for word in out:
+        # word의 형태는 ("단어", "품사") 이므로 word[1]은 품사를 나타내고 word[0]는 단어를 나타냄.
+        if word[1] in meaning_tags:
+            meaning_words.append(word[0])
+    return meaning_words
